bac77220fb5432a87c8a4adfca7d264ce3d6ee26
elias54 <elias5401@hotmail.fr>
2013-06-07 22:58:40 +0200

    Micro reorganization in the main class

--- src/minecraft/fr/mcnanotech/FFMT/FFMTAPI/FFMTAPI.java
+++ src/minecraft/fr/mcnanotech/FFMT/FFMTAPI/FFMTAPI.java
@@ -3,10 +3,9 @@ package fr.mcnanotech.FFMT.FFMTAPI;
 import cpw.mods.fml.common.Mod;
 import cpw.mods.fml.common.network.NetworkMod;
 
-
 @Mod(modid = "FFMTAPI", name = "FFMT API", version = "1.0.0")
 @NetworkMod(clientSideRequired = true, serverSideRequired = false)
 public class FFMTAPI 
 {
-
+	
 }
