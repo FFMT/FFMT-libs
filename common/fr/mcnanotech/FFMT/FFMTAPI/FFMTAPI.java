60a94e2c6cb0fbc716cc4ce7b9dafc90875181f4
robin <mc-robin4002@gmx.fr>
2013-07-17 21:12:34 +0200

    Remove FFMT API mod test, update for 1.6.2

--- common/fr/mcnanotech/FFMT/FFMTAPI/FFMTAPI.java
+++ common/fr/mcnanotech/FFMT/FFMTAPI/FFMTAPI.java
@@ -6,6 +6,7 @@ import java.util.logging.Logger;
 
 import cpw.mods.fml.common.FMLLog;
 import cpw.mods.fml.common.Mod;
+import cpw.mods.fml.common.Mod.EventHandler;
 import cpw.mods.fml.common.Mod.Init;
 import cpw.mods.fml.common.Mod.Metadata;
 import cpw.mods.fml.common.Mod.PreInit;
@@ -24,30 +25,19 @@ import fr.mcnanotech.FFMT.FFMTAPI.Interfaces.NotWorking;
 public class FFMTAPI 
 {
 	public static Logger FFMTlog;
-	@Metadata("FFMTAPI")
-	public static ModMetadata meta;
 	public static FFMTColor getColor = new FFMTColor();
 	
-	@PreInit
+	@EventHandler
 	public void preload(FMLPreInitializationEvent event)
 	{
 		FFMTlog = FMLLog.getLogger();
-		FFMTlog.log(Level.INFO, "PreInitialization");
+		event.getModMetadata().version = "1.0.1";
 	}
 	
-	@NotWorking
-	@Init
+	@EventHandler
 	public void load(FMLInitializationEvent event)
 	{
 
-		FFMTlog.log(Level.INFO, "Initialization");
-		meta.modId       = "FFMTAPI";
-		meta.name        = "FFMT API";
-		meta.version     = "1.0.0";
-		meta.authorList  = Arrays.asList(new String[] {"kevin_68", "robin4002", "elias54"});
-		meta.description = "simplify your coder life";
-		meta.url         = "http://forge.mcnanotech.fr/";
-		meta.logoFile    = "/ffmt_logo.png";
 	}
 	
 }
