6feab6a7675af51a43cff25d6f1ae86a472e72f5
robin <robin4002@gmail.com>
2013-08-17 20:43:33 +0200

    Added baseleave

---
+++ common/fr/minecraftforgefrance/ffmtapi/blockhelper/BlockFFMTLeavesBase.java
@@ -0,0 +1,43 @@
+package fr.minecraftforgefrance.ffmtapi.blockhelper;
+
+import java.util.List;
+
+import net.minecraft.block.Block;
+import net.minecraft.block.BlockLeaves;
+import net.minecraft.creativetab.CreativeTabs;
+import net.minecraft.item.ItemStack;
+import net.minecraft.util.Icon;
+import net.minecraft.world.IBlockAccess;
+import cpw.mods.fml.relauncher.Side;
+import cpw.mods.fml.relauncher.SideOnly;
+
+public class BlockFFMTLeavesBase extends BlockLeaves
+{
+	protected Icon fastIcon;
+
+	protected BlockFFMTLeavesBase(int id)
+	{
+		super(id);
+	}
+
+	public boolean isOpaqueCube()
+	{
+		return Block.leaves.isOpaqueCube();
+	}
+
+	@SideOnly(Side.CLIENT)
+	public boolean shouldSideBeRendered(IBlockAccess blockaccess, int x, int y, int z, int side)
+	{
+		return !this.isOpaqueCube() ? true : super.shouldSideBeRendered(blockaccess, x, y, z, side);
+	}
+
+	public Icon getIcon(int side, int metadata)
+	{
+		return(isOpaqueCube() ? fastIcon : blockIcon);
+	}
+
+	public void getSubBlocks(int par1, CreativeTabs creativeTabs, List list)
+	{
+		list.add(new ItemStack(par1, 1, 0));
+	}
+}
