469d862293aaf02d9d5a59542a14435232aaa6be
robin <robin4002@gmail.com>
2013-12-04 19:36:55 +0100

    Fix typo, second constructor to define slider size

--- common/fr/minecraftforgefrance/ffmtlibs/gui/GuiSliderForContainer.java
+++ common/fr/minecraftforgefrance/ffmtlibs/gui/GuiSliderForContainer.java
@@ -11,15 +11,20 @@ import cpw.mods.fml.relauncher.SideOnly;
 @SideOnly(Side.CLIENT)
 public class GuiSliderForContainer extends GuiButton
 {
-	public float sliderValue;
+	public float sladerValue;
 	public boolean dragging;
 	private int sladerId;
 	private GuiContainerSliderBase container;
 
 	public GuiSliderForContainer(GuiContainerSliderBase containerSliderBase, int id, int x, int y, String name, float value)
 	{
-		super(id, x, y, 150, 20, name);
-		this.sliderValue = value;
+		this(containerSliderBase, id, x, y, 150, 20, name, value);
+	}
+	
+	public GuiSliderForContainer(GuiContainerSliderBase containerSliderBase, int id, int x, int y, int xSize, int ySize, String name, float value)
+	{
+		super(id, x, y, xSize, ySize, name);
+		this.sladerValue = value;
 		this.sladerId = id;
 		this.container = containerSliderBase;
 	}
@@ -35,25 +40,25 @@ public class GuiSliderForContainer extends GuiButton
 		{
 			if(this.dragging)
 			{
-				this.sliderValue = (float)(x - (this.xPosition + 4)) / (float)(this.width - 8);
+				this.sladerValue = (float)(x - (this.xPosition + 4)) / (float)(this.width - 8);
 
-				if(this.sliderValue < 0.0F)
+				if(this.sladerValue < 0.0F)
 				{
-					this.sliderValue = 0.0F;
+					this.sladerValue = 0.0F;
 				}
 
-				if(this.sliderValue > 1.0F)
+				if(this.sladerValue > 1.0F)
 				{
-					this.sliderValue = 1.0F;
+					this.sladerValue = 1.0F;
 				}
 
-				container.handlerSliderAction(this.sladerId, this.sliderValue);
-				this.displayString = container.getSladerName(this.sladerId, this.sliderValue);
+				container.handlerSliderAction(this.sladerId, this.sladerValue);
+				this.displayString = container.getSladerName(this.sladerId, this.sladerValue);
 			}
 
 			GL11.glColor4f(1.0F, 1.0F, 1.0F, 1.0F);
-			this.drawTexturedModalRect(this.xPosition + (int)(this.sliderValue * (float)(this.width - 8)), this.yPosition, 0, 66, 4, 20);
-			this.drawTexturedModalRect(this.xPosition + (int)(this.sliderValue * (float)(this.width - 8)) + 4, this.yPosition, 196, 66, 4, 20);
+			this.drawTexturedModalRect(this.xPosition + (int)(this.sladerValue * (float)(this.width - 8)), this.yPosition, 0, 66, 4, 20);
+			this.drawTexturedModalRect(this.xPosition + (int)(this.sladerValue * (float)(this.width - 8)) + 4, this.yPosition, 196, 66, 4, 20);
 		}
 	}
 
@@ -61,20 +66,20 @@ public class GuiSliderForContainer extends GuiButton
 	{
 		if(super.mousePressed(mc, x, y))
 		{
-			this.sliderValue = (float)(x - (this.xPosition + 4)) / (float)(this.width - 8);
+			this.sladerValue = (float)(x - (this.xPosition + 4)) / (float)(this.width - 8);
 
-			if(this.sliderValue < 0.0F)
+			if(this.sladerValue < 0.0F)
 			{
-				this.sliderValue = 0.0F;
+				this.sladerValue = 0.0F;
 			}
 
-			if(this.sliderValue > 1.0F)
+			if(this.sladerValue > 1.0F)
 			{
-				this.sliderValue = 1.0F;
+				this.sladerValue = 1.0F;
 			}
 
-			container.handlerSliderAction(this.sladerId, this.sliderValue);
-			this.displayString = container.getSladerName(this.sladerId, this.sliderValue);
+			container.handlerSliderAction(this.sladerId, this.sladerValue);
+			this.displayString = container.getSladerName(this.sladerId, this.sladerValue);
 			this.dragging = true;
 			return true;
 		}
