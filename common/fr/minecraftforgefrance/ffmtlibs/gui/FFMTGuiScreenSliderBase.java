8897b3874db81e5dd49a96902218b8269f75d99e
kevin68 <baumeyerkevin@gmx.fr>
2014-03-12 14:36:06 +0100

    Add sliders for GuiScreen

---
+++ common/fr/minecraftforgefrance/ffmtlibs/gui/FFMTGuiScreenSliderBase.java
@@ -0,0 +1,48 @@
+package fr.minecraftforgefrance.ffmtlibs.gui;
+
+import net.minecraft.client.audio.PositionedSoundRecord;
+import net.minecraft.client.gui.GuiButton;
+import net.minecraft.client.gui.GuiScreen;
+import net.minecraft.inventory.Container;
+import net.minecraft.util.ResourceLocation;
+import cpw.mods.fml.relauncher.Side;
+import cpw.mods.fml.relauncher.SideOnly;
+
+@SideOnly(Side.CLIENT)
+public abstract class FFMTGuiScreenSliderBase extends GuiScreen
+{
+	private GuiButton selectedButton;
+
+	protected void mouseClicked(int x, int y, int id)
+	{
+		super.mouseClicked(x, y, id);
+		if(id == 0)
+		{
+			for(int l = 0; l < this.buttonList.size(); ++l)
+			{
+				GuiButton guibutton = (GuiButton)this.buttonList.get(l);
+
+				if(guibutton.mousePressed(this.mc, x, y) && guibutton instanceof FFMTGuiSliderForContainer)
+				{
+					this.selectedButton = guibutton;
+					this.mc.getSoundHandler().playSound(PositionedSoundRecord.func_147674_a(new ResourceLocation("gui.button.press"), 1.0F));
+					this.actionPerformed(guibutton);
+				}
+			}
+		}
+	}
+
+	protected void mouseMovedOrUp(int x, int y, int id)
+	{
+		super.mouseMovedOrUp(x, y, id);
+		if(this.selectedButton != null && id == 0)
+		{
+			this.selectedButton.mouseReleased(x, y);
+			this.selectedButton = null;
+		}
+	}
+
+	public abstract void handlerSliderAction(int sliderId, float sliderValue);
+
+	public abstract String getSliderName(int sliderId, float sliderValue);
+}
