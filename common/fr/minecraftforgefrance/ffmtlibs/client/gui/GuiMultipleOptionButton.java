506b01bd082b68c98e4d03a3c0864f446d47f821
kevin68 <baumeyerkevin@gmx.fr>
2014-07-31 01:16:34 +0200

    Option for other textures

--- common/fr/minecraftforgefrance/ffmtlibs/client/gui/GuiMultipleOptionButton.java
+++ common/fr/minecraftforgefrance/ffmtlibs/client/gui/GuiMultipleOptionButton.java
@@ -1,86 +1,99 @@
 package fr.minecraftforgefrance.ffmtlibs.client.gui;
 
-import java.util.ArrayList;
-
-import org.lwjgl.opengl.GL11;
-
 import net.minecraft.client.Minecraft;
 import net.minecraft.client.gui.FontRenderer;
 import net.minecraft.client.gui.GuiButton;
+import net.minecraft.client.renderer.OpenGlHelper;
 import net.minecraft.util.ResourceLocation;
+
+import org.lwjgl.opengl.GL11;
 
 public class GuiMultipleOptionButton extends GuiButton
 {
-	protected static final ResourceLocation buttonTextures = new ResourceLocation("textures/gui/widgets.png");
-	private final String[] changTxt;
-	private int currentState;
-	
-	public GuiMultipleOptionButton(int id, int x, int y, String text, String[] changingTxt, int currentState)
-	{
-		this(id, x, y, 150, 20, text, changingTxt, currentState);
-	}
+    protected ResourceLocation buttonTex;
+    private final String[] changTxt;
+    private int currentState, yTex;
+    private boolean useHoverState;
 
-	public GuiMultipleOptionButton(int id, int x, int y, int width, int height, String s, String[] changingTxt, int currentState)
-	{
-		super(id, x, y, width, height, s);
-		this.changTxt = changingTxt;
-		this.currentState = currentState;
-	}
-	
-	public void next()
-	{
-		currentState++;
-		if(currentState >= changTxt.length)
-		{
-			currentState = 0;
-		}
-	}
-	
-	public int getState()
-	{
-		return currentState;
-	}
-	
-	public int getHoverState(boolean mouseIsInButton)
-	{
-		byte b0 = 1;
+    public GuiMultipleOptionButton(int id, int x, int y, String text, String[] changingTxt, int currentState)
+    {
+        this(id, x, y, 150, 20, text, changingTxt, currentState);
+    }
 
-		if(!this.enabled)
-		{
-			b0 = 0;
-		}
-		else if(mouseIsInButton)
-		{
-			b0 = 2;
-		}
+    public GuiMultipleOptionButton(int id, int x, int y, int width, int height, String s, String[] changingTxt, int currentState)
+    {
+        this(id, x, y, width, height, s, changingTxt, currentState, new ResourceLocation("textures/gui/widgets.png"), 46, true);
+    }
 
-		return b0;
-	}
-	
-	public void drawButton(Minecraft mc, int x, int y)
-	{
-		if(this.visible)
-		{
-			FontRenderer fontrenderer = mc.fontRenderer;
-			mc.getTextureManager().bindTexture(buttonTextures);
-			GL11.glColor4f(1.0F, 1.0F, 1.0F, 1.0F);
-			this.field_146123_n = x >= this.xPosition && y >= this.yPosition && x < this.xPosition + this.width && y < this.yPosition + this.height;
-			int k = this.getHoverState(this.field_146123_n);
-			this.drawTexturedModalRect(this.xPosition, this.yPosition, 0, 46 + k * 20, this.width / 2, this.height);
-			this.drawTexturedModalRect(this.xPosition + this.width / 2, this.yPosition, 200 - this.width / 2, 46 + k * 20, this.width / 2, this.height);
-			this.mouseDragged(mc, x, y);
-			int l = 14737632;
-			String str = this.displayString + this.changTxt[currentState];
-			if(!this.enabled)
-			{
-				l = -6250336;
-			}
-			else if(this.field_146123_n)
-			{
-				l = 16777120;
-			}
+    public GuiMultipleOptionButton(int id, int x, int y, int width, int height, String s, String[] changingTxt, int currentState, ResourceLocation custom, int yTex, boolean useHoverState)
+    {
+        super(id, x, y, width, height, s);
+        this.changTxt = changingTxt;
+        this.currentState = currentState;
+        this.buttonTex = custom;
+        this.yTex = yTex;
+        this.useHoverState = useHoverState;
+    }
 
-			this.drawCenteredString(fontrenderer, str, this.xPosition + this.width / 2, this.yPosition + (this.height - 8) / 2, l);
-		}
-	}
+    public void next()
+    {
+        currentState++;
+        if(currentState >= changTxt.length)
+        {
+            currentState = 0;
+        }
+    }
+
+    public int getState()
+    {
+        return currentState;
+    }
+
+    @Override
+    public int getHoverState(boolean mouseIsInButton)
+    {
+        byte b0 = 1;
+
+        if(!this.enabled)
+        {
+            b0 = 0;
+        }
+        else if(mouseIsInButton)
+        {
+            b0 = 2;
+        }
+
+        return b0;
+    }
+
+    @Override
+    public void drawButton(Minecraft mc, int x, int y)
+    {
+        if(this.visible)
+        {
+            FontRenderer fontrenderer = mc.fontRenderer;
+            mc.getTextureManager().bindTexture(buttonTex);
+            GL11.glColor4f(1.0F, 1.0F, 1.0F, 1.0F);
+            this.field_146123_n = x >= this.xPosition && y >= this.yPosition && x < this.xPosition + this.width && y < this.yPosition + this.height;
+            int k = this.getHoverState(this.field_146123_n);
+            GL11.glEnable(GL11.GL_BLEND);
+            OpenGlHelper.glBlendFunc(770, 771, 1, 0);
+            GL11.glBlendFunc(GL11.GL_SRC_ALPHA, GL11.GL_ONE_MINUS_SRC_ALPHA);
+            this.drawTexturedModalRect(this.xPosition, this.yPosition, 0, yTex + (useHoverState ? (k * 20) : 0), this.width / 2, this.height);
+            this.drawTexturedModalRect(this.xPosition + this.width / 2, this.yPosition, 200 - this.width / 2, yTex + (useHoverState ? (k * 20) : 0), this.width / 2, this.height);
+            this.mouseDragged(mc, x, y);
+            int l = 14737632;
+            String str = this.displayString + this.changTxt[currentState];
+            if(!this.enabled)
+            {
+                l = -6250336;
+            }
+            else if(this.field_146123_n)
+            {
+                l = 16777120;
+            }
+
+            this.drawCenteredString(fontrenderer, str, this.xPosition + this.width / 2, this.yPosition + (this.height - 8) / 2, l);
+        }
+    }
 }
--- common/fr/minecraftforgefrance/ffmtlibs/client/gui/GuiSliderForScreen.java
+++ common/fr/minecraftforgefrance/ffmtlibs/client/gui/GuiSliderForScreen.java
@@ -1,7 +1,10 @@
 package fr.minecraftforgefrance.ffmtlibs.client.gui;
 
 import net.minecraft.client.Minecraft;
+import net.minecraft.client.gui.FontRenderer;
 import net.minecraft.client.gui.GuiButton;
+import net.minecraft.client.renderer.OpenGlHelper;
+import net.minecraft.util.ResourceLocation;
 
 import org.lwjgl.opengl.GL11;
 
@@ -11,96 +14,149 @@ import cpw.mods.fml.relauncher.SideOnly;
 @SideOnly(Side.CLIENT)
 public class GuiSliderForScreen extends GuiButton
 {
-	public float sliderValue;
-	public boolean dragging;
-	private int sliderId;
-	private GuiScreenSliderBase container;
+    private ResourceLocation texture;
+    public float sliderValue;
+    public boolean dragging;
+    private int sliderId, yTexBackGround;
+    private GuiScreenSliderBase container;
 
-	public GuiSliderForScreen(GuiScreenSliderBase containerSliderBase, int id, int x, int y, String name, float value)
-	{
-		this(containerSliderBase, id, x, y, 150, 20, name, value);
-	}
+    public GuiSliderForScreen(GuiScreenSliderBase containerSliderBase, int id, int x, int y, String name, float value)
+    {
+        this(containerSliderBase, id, x, y, 150, 20, name, value);
+    }
 
-	public GuiSliderForScreen(GuiScreenSliderBase containerSliderBase, int id, int x, int y, int xSize, int ySize, String name, float value)
-	{
-		super(id, x, y, xSize, ySize, name);
-		this.sliderValue = value;
-		this.sliderId = id;
-		this.container = containerSliderBase;
-	}
+    public GuiSliderForScreen(GuiScreenSliderBase containerSliderBase, int id, int x, int y, int xSize, int ySize, String name, float value)
+    {
+        this(containerSliderBase, id, x, y, xSize, ySize, name, value, new ResourceLocation("textures/gui/widgets.png"), -1);
+    }
 
-	public void disable()
-	{
-		this.enabled = false;
-	}
+    public GuiSliderForScreen(GuiScreenSliderBase containerSliderBase, int id, int x, int y, int xSize, int ySize, String name, float value, ResourceLocation customTexture, int yTexBackGround)
+    {
+        super(id, x, y, xSize, ySize, name);
+        this.sliderValue = value;
+        this.sliderId = id;
+        this.container = containerSliderBase;
+        this.texture = customTexture;
+        this.yTexBackGround = yTexBackGround;
+    }
 
-	public void enable()
-	{
-		this.enabled = true;
-	}
+    public void disable()
+    {
+        this.enabled = false;
+    }
 
-	public int getHoverState(boolean b)
-	{
-		return 0;
-	}
+    public void enable()
+    {
+        this.enabled = true;
+    }
 
-	protected void mouseDragged(Minecraft mc, int x, int y)
-	{
-		if(this.visible)
-		{
-			if(this.dragging)
-			{
-				this.sliderValue = (float)(x - (this.xPosition + 4)) / (float)(this.width - 8);
+    @Override
+    public int getHoverState(boolean b)
+    {
+        return 0;
+    }
 
-				if(this.sliderValue < 0.0F)
-				{
-					this.sliderValue = 0.0F;
-				}
+    @Override
+    protected void mouseDragged(Minecraft mc, int x, int y)
+    {
+        if(this.visible)
+        {
+            if(this.dragging)
+            {
+                this.sliderValue = (float)(x - (this.xPosition + 4)) / (float)(this.width - 8);
 
-				if(this.sliderValue > 1.0F)
-				{
-					this.sliderValue = 1.0F;
-				}
+                if(this.sliderValue < 0.0F)
+                {
+                    this.sliderValue = 0.0F;
+                }
 
-				container.handlerSliderAction(this.sliderId, this.sliderValue);
-				this.displayString = container.getSliderName(this.sliderId, this.sliderValue);
-			}
+                if(this.sliderValue > 1.0F)
+                {
+                    this.sliderValue = 1.0F;
+                }
 
-			GL11.glColor4f(1.0F, 1.0F, 1.0F, 1.0F);
-			this.drawTexturedModalRect(this.xPosition + (int)(this.sliderValue * (float)(this.width - 8)), this.yPosition, 0, 66, 4, 20);
-			this.drawTexturedModalRect(this.xPosition + (int)(this.sliderValue * (float)(this.width - 8)) + 4, this.yPosition, 196, 66, 4, 20);
-		}
-	}
+                container.handlerSliderAction(this.sliderId, this.sliderValue);
+                this.displayString = container.getSliderName(this.sliderId, this.sliderValue);
+            }
 
-	public boolean mousePressed(Minecraft mc, int x, int y)
-	{
-		if(super.mousePressed(mc, x, y))
-		{
-			this.sliderValue = (float)(x - (this.xPosition + 4)) / (float)(this.width - 8);
+            GL11.glColor4f(1.0F, 1.0F, 1.0F, 1.0F);
+            mc.getTextureManager().bindTexture(texture);
+            this.drawTexturedModalRect(this.xPosition + (int)(this.sliderValue * (this.width - 8)), this.yPosition, 0, yTexBackGround == -1 ? 66 : yTexBackGround, 4, 20);
+            this.drawTexturedModalRect(this.xPosition + (int)(this.sliderValue * (this.width - 8)) + 4, this.yPosition, 196, yTexBackGround == -1 ? 66 : yTexBackGround, 4, 20);
+        }
+    }
 
-			if(this.sliderValue < 0.0F)
-			{
-				this.sliderValue = 0.0F;
-			}
+    @Override
+    public boolean mousePressed(Minecraft mc, int x, int y)
+    {
+        if(super.mousePressed(mc, x, y))
+        {
+            this.sliderValue = (float)(x - (this.xPosition + 4)) / (float)(this.width - 8);
 
-			if(this.sliderValue > 1.0F)
-			{
-				this.sliderValue = 1.0F;
-			}
+            if(this.sliderValue < 0.0F)
+            {
+                this.sliderValue = 0.0F;
+            }
 
-			container.handlerSliderAction(this.sliderId, this.sliderValue);
-			this.displayString = container.getSliderName(this.sliderId, this.sliderValue);
-			this.dragging = true;
-			return true;
-		}
-		else
-		{
-			return false;
-		}
-	}
+            if(this.sliderValue > 1.0F)
+            {
+                this.sliderValue = 1.0F;
+            }
 
-	public void mouseReleased(int x, int y)
-	{
-		this.dragging = false;
-	}
+            container.handlerSliderAction(this.sliderId, this.sliderValue);
+            this.displayString = container.getSliderName(this.sliderId, this.sliderValue);
+            this.dragging = true;
+            return true;
+        }
+        else
+        {
+            return false;
+        }
+    }
+
+    @Override
+    public void mouseReleased(int x, int y)
+    {
+        this.dragging = false;
+    }
+
+    @Override
+    public void drawButton(Minecraft mc, int mouseX, int mouseY)
+    {
+        if(this.visible && yTexBackGround != -1)
+        {
+            FontRenderer fontrenderer = mc.fontRenderer;
+            mc.getTextureManager().bindTexture(texture);
+            GL11.glColor4f(1.0F, 1.0F, 1.0F, 1.0F);
+            this.field_146123_n = mouseX >= this.xPosition && mouseY >= this.yPosition && mouseX < this.xPosition + this.width && mouseY < this.yPosition + this.height;
+            int k = this.getHoverState(this.field_146123_n);
+            GL11.glEnable(GL11.GL_BLEND);
+            OpenGlHelper.glBlendFunc(770, 771, 1, 0);
+            GL11.glBlendFunc(GL11.GL_SRC_ALPHA, GL11.GL_ONE_MINUS_SRC_ALPHA);
+
+            this.drawTexturedModalRect(this.xPosition, this.yPosition, 0, yTexBackGround, this.width / 2, yTexBackGround + this.height);
+            this.drawTexturedModalRect(this.xPosition + this.width / 2, this.yPosition, 200 - this.width / 2, yTexBackGround, this.width / 2, yTexBackGround + this.height);
+
+            int l = 14737632;
+
+            if(packedFGColour != 0)
+            {
+                l = packedFGColour;
+            }
+            else if(!this.enabled)
+            {
+                l = 10526880;
+            }
+            else if(this.field_146123_n)
+            {
+                l = 16777120;
+            }
+            this.drawString(fontrenderer, this.displayString, this.xPosition + 5, this.yPosition + (this.height - 8) / 2, l);
+            this.mouseDragged(mc, mouseX, mouseY);
+        }
+        else
+        {
+            super.drawButton(mc, mouseX, mouseY);
+        }
+    }
 }
