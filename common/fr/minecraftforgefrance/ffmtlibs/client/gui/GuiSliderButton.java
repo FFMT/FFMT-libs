14882b25b39345eb51625dc7f496a600b8c8a5e3
robin4002 <robin4002@gmail.com>
2014-12-14 21:00:56 +0100

    add missing constructor

--- common/fr/minecraftforgefrance/ffmtlibs/client/gui/GuiSliderButton.java
+++ common/fr/minecraftforgefrance/ffmtlibs/client/gui/GuiSliderButton.java
@@ -4,89 +4,98 @@ import net.minecraft.client.Minecraft;
 import net.minecraft.client.gui.GuiButton;
 import net.minecraft.client.renderer.GlStateManager;
 import net.minecraft.util.MathHelper;
+import net.minecraft.util.ResourceLocation;
 import net.minecraftforge.fml.relauncher.Side;
 import net.minecraftforge.fml.relauncher.SideOnly;
 
 @SideOnly(Side.CLIENT)
 public class GuiSliderButton extends GuiButton
 {
-	public float sliderValue;
-	public boolean dragging;
-	private int sliderId;
-	private ISliderButton iSliderButton;
+    public float sliderValue;
+    public boolean dragging;
+    private int sliderId, yTexBackGround;
+    private ISliderButton iSliderButton;
+    private ResourceLocation texture;
 
-	public GuiSliderButton(ISliderButton containerSliderBase, int id, int x, int y, String name, float value)
-	{
-		this(containerSliderBase, id, x, y, 150, 20, name, value);
-	}
+    public GuiSliderButton(ISliderButton containerSliderBase, int id, int x, int y, String name, float value)
+    {
+        this(containerSliderBase, id, x, y, 150, 20, name, value);
+    }
 
-	public GuiSliderButton(ISliderButton containerSliderBase, int id, int x, int y, int xSize, int ySize, String name, float value)
-	{
-		super(id, x, y, xSize, ySize, name);
-		this.sliderValue = value;
-		this.sliderId = id;
-		this.iSliderButton = containerSliderBase;
-	}
+    public GuiSliderButton(ISliderButton containerSliderBase, int id, int x, int y, int xSize, int ySize, String name, float value)
+    {
+        this(containerSliderBase, id, x, y, xSize, ySize, name, value, buttonTextures, -1);
+    }
 
-	public void disable()
-	{
-		this.enabled = false;
-	}
+    public GuiSliderButton(ISliderButton containerSliderBase, int id, int x, int y, int xSize, int ySize, String name, float value, ResourceLocation customTexture, int yTexBackGround)
+    {
+        super(id, x, y, xSize, ySize, name);
+        this.sliderValue = value;
+        this.sliderId = id;
+        this.iSliderButton = containerSliderBase;
+        this.texture = customTexture;
+        this.yTexBackGround = yTexBackGround;
+    }
 
-	public void enable()
-	{
-		this.enabled = true;
-	}
+    public void disable()
+    {
+        this.enabled = false;
+    }
 
-	@Override
-	public int getHoverState(boolean mouseOver)
-	{
-		return 0;
-	}
+    public void enable()
+    {
+        this.enabled = true;
+    }
 
-	@Override
-	protected void mouseDragged(Minecraft mc, int mouseX, int mouseY)
-	{
-		if(this.visible)
-		{
-			if(this.dragging)
-			{
-				this.sliderValue = (float)(mouseX - (this.xPosition + 4)) / (float)(this.width - 8);
-				this.sliderValue = MathHelper.clamp_float(this.sliderValue, 0.0F, 1.0F);
+    @Override
+    public int getHoverState(boolean mouseOver)
+    {
+        return 0;
+    }
 
-				iSliderButton.handlerSliderAction(this.sliderId, this.sliderValue);
-				this.displayString = iSliderButton.getSliderName(this.sliderId, this.sliderValue);
-			}
+    @Override
+    protected void mouseDragged(Minecraft mc, int mouseX, int mouseY)
+    {
+        if(this.visible)
+        {
+            if(this.dragging)
+            {
+                this.sliderValue = (float)(mouseX - (this.xPosition + 4)) / (float)(this.width - 8);
+                this.sliderValue = MathHelper.clamp_float(this.sliderValue, 0.0F, 1.0F);
 
-			mc.getTextureManager().bindTexture(buttonTextures);
-			GlStateManager.color(1.0F, 1.0F, 1.0F, 1.0F);
-			this.drawTexturedModalRect(this.xPosition + (int)(this.sliderValue * (this.width - 8)), this.yPosition, 0, 66, 4, 20);
-			this.drawTexturedModalRect(this.xPosition + (int)(this.sliderValue * (this.width - 8)) + 4, this.yPosition, 196, 66, 4, 20);
-		}
-	}
+                iSliderButton.handlerSliderAction(this.sliderId, this.sliderValue);
+                this.displayString = iSliderButton.getSliderName(this.sliderId, this.sliderValue);
+            }
 
-	@Override
-	public boolean mousePressed(Minecraft mc, int mouseX, int mouseY)
-	{
-		if(super.mousePressed(mc, mouseX, mouseY))
-		{
-			this.sliderValue = (float)(mouseX - (this.xPosition + 4)) / (float)(this.width - 8);
-			this.sliderValue = MathHelper.clamp_float(this.sliderValue, 0.0F, 1.0F);
+            mc.getTextureManager().bindTexture(texture);
+            GlStateManager.color(1.0F, 1.0F, 1.0F, 1.0F);
+            this.drawTexturedModalRect(this.xPosition + (int)(this.sliderValue * (this.width - 8)), this.yPosition, 0, yTexBackGround == -1 ? 66 : yTexBackGround, 4, 20);
+            this.drawTexturedModalRect(this.xPosition + (int)(this.sliderValue * (this.width - 8)) + 4, this.yPosition, 196, yTexBackGround == -1 ? 66 : yTexBackGround, 4, 20);
+        }
+    }
 
-			iSliderButton.handlerSliderAction(this.sliderId, this.sliderValue);
-			this.displayString = iSliderButton.getSliderName(this.sliderId, this.sliderValue);
-			this.dragging = true;
-			return true;
-		}
-		else
-		{
-			return false;
-		}
-	}
+    @Override
+    public boolean mousePressed(Minecraft mc, int mouseX, int mouseY)
+    {
+        if(super.mousePressed(mc, mouseX, mouseY))
+        {
+            this.sliderValue = (float)(mouseX - (this.xPosition + 4)) / (float)(this.width - 8);
+            this.sliderValue = MathHelper.clamp_float(this.sliderValue, 0.0F, 1.0F);
 
-	@Override
-	public void mouseReleased(int mouseX, int mouseY)
-	{
-		this.dragging = false;
-	}
+            iSliderButton.handlerSliderAction(this.sliderId, this.sliderValue);
+            this.displayString = iSliderButton.getSliderName(this.sliderId, this.sliderValue);
+            this.dragging = true;
+            return true;
+        }
+        else
+        {
+            return false;
+        }
+    }
+
+    @Override
+    public void mouseReleased(int mouseX, int mouseY)
+    {
+        this.dragging = false;
+    }
 }
